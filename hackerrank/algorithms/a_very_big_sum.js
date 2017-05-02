9b0328c57c683d12d53efc83eb247611a4c167f5
Reuben Jones <mrreubenjones@gmail.com>
2017-05-02 16:31:56 -0700

    Solve 'A Very Big Sum'

---
+++ hackerrank/algorithms/a_very_big_sum.js
@@ -0,0 +1,36 @@
+// A Very Big Sum
+// https://www.hackerrank.com/challenges/a-very-big-sum
+// May 01, 2017
+
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+
+function main() {
+    var n = parseInt(readLine());
+    arr = readLine().split(' ');
+    arr = arr.map(Number);
+
+    var sum = arr.reduce(function(x, y){
+        return x + y;
+    });
+    console.log(sum);
+}
